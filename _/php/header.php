032f7306925cb179565958135729a88ac887dd57
fdiazsmith <risd@Fernandos-MacBook-Pro.local>
2014-03-22 16:33:31 -0400

    first commit

---
+++ _/php/header.php
@@ -0,0 +1,9 @@
+<header class="nav-bar">
+      <img class="" src="imgs/logo.jpg">
+      <ul>
+        <li class="nav-button"><a href="servicios.html">Servicios</a></li>
+        <li class="nav-button"><a href="metodologia.html">Metodología</a></li>
+        <li class="nav-button"><a href="ana-luz.html">Ana Luz</a></li>
+        <li class="nav-button"><a href="english.html">English</a></li>
+      </ul>
+</header>
---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="bower_components/normalize-css/normalize.css">
+        <link rel="stylesheet" href="_/css/bootstrap.css">
+        <link rel="stylesheet" href="_/css/main.css">
+         <!-- <script src="js/vendor/modernizr-2.6.2.min.js"></script> -->
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+       
+        <?php include '_/php/header.php';?>
+         <p>Hello world! This is HTML5 Boilerplate.</p>
+        <h1>hola</h1>
+        <h1>hola</h1><h1>hola</h1><h1>hola</h1><h1>hola</h1><h1>hola</h1><h1>hola</h1><h1>adios</h1>
+        
+        <script src="_/js/allScripts-ck.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID.
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+         -->
+    </body>
+</html>
