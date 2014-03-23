fc0e71a0ff082aef882d467084b7fbe19685efd5
fdiazsmith <risd@Fernandos-MacBook-Pro.local>
2014-03-23 18:21:33 -0400

    handles the logo at different sizes

--- _/php/header.php
+++ _/php/header.php
@@ -1,24 +1,28 @@
-<header class="navbar  navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+<header class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <div class="brand"> 
+        
+        <a class="navbar-brand" href="#"><span class="Al">AL</span><span class="Smith">Smith</span> <span class="associados">y Associados</span></a>
+      </div>
+      
+    </div>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse navbar-right " id="nav-links">
       <ul class="nav navbar-nav navbar-left ">
         <li class="nav-button "><a href="#"> <span class="glyphicon glyphicon-pause"></span>Servicios</a></li>
         <li class="nav-button"><a href="#">Metodología</a></li>
         <li class="nav-button"><a href="#">Ana Luz</a></li>
         <li class="nav-button"><a href="#">English</a></li>
-
       </ul>
     </div> <!-- /.navbar-collapse -->
-      </div>
-    </header>
+  </div>
+</header>
+
 
