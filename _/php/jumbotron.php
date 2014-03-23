d8a64b7dbe9c181fb01466e857b57d188da2944c
fdiazsmith <risd@Fernandos-MacBook-Pro.local>
2014-03-23 19:05:01 -0400

    introducing jumbotron as an element

---
+++ _/php/jumbotron.php
@@ -0,0 +1,11 @@
+<div class="jumbotron  ">
+  <div class="container hidden-xs" >
+    <p class="frases">Ningún hombre es una isla, <i class="">algo completo en si mismo;</i><br>
+      todo hombre es un fragmento del continente. Una parte de un conjunto
+    </p>
+    <p class="autor">  <span class="autor">John Donne</span>
+    </p>
+    
+    <p><a class="btn btn-primary btn-lg" role="button">Learn more »</a></p>
+  </div>
+</div>
