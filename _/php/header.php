40a7e629142b16e9cb9ab0cbee540fff56730876
fdiazsmith <risd@Fernandos-MacBook-Pro.local>
2014-03-23 17:01:09 -0400

    solves nag button not showing up in small screens

--- _/php/header.php
+++ _/php/header.php
@@ -1,14 +1,14 @@
-<header class="navbar nav-bar navbar-fixed-top" role="navigation">
-  <div class="container-fluid">
-    <div class="navbar-header ">
-      <button type="button" class="navbar-toggle " data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="glyphicon glyphicon-list" style="font-size:20px;"></span>
-      </button>
-      <!-- <img class="inavbar-brand mg-responsive" src="imgs/logo.jpg"> -->
-       <a class="navbar-brand" href="#">Alsmith</a>
-    </div>
-
+<header class="navbar  navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-left ">
@@ -19,5 +19,6 @@
 
       </ul>
     </div> <!-- /.navbar-collapse -->
-  </div> <!-- /.container fluid -->
-</header>
+      </div>
+    </header>
+
