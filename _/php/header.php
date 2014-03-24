0e93c7825c97793dab4bca5646e9045a5982e063
fdiazsmith <risd@Fernandos-MacBook-Pro.local>
2014-03-23 22:49:01 -0400

    adds metodologia and servicios

--- _/php/header.php
+++ _/php/header.php
@@ -7,17 +7,18 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <div class="brand"> 
-        
-        <a class="navbar-brand" href="#"><span class="Al">AL</span><span class="Smith">Smith</span> <span class="associados">y Associados</span></a>
-      </div>
+      <a href="index.php">
+        <div class="brand" >   
+          <div class="navbar-brand als" href="#"><span class="Al">AL</span><span class="Smith">Smith</span> <span class="associados">y Associados</span></div>
+        </div>
+      </a>
       
     </div>
       <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse navbar-right " id="nav-links">
       <ul class="nav navbar-nav navbar-left ">
-        <li class="nav-button "><a href="#">Servicios</a></li>
-        <li class="nav-button"><a href="#">Metodología</a></li>
+        <li class="nav-button "><a href="servicios.php">Servicios</a></li>
+        <li class="nav-button"><a href="metodologia.php">Metodología</a></li>
         <li class="nav-button"><a href="#">Ana Luz</a></li>
         <li class="nav-button"><a href="#">English</a></li>
       </ul>
---
+++ servicios.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- <link rel="shortcut icon" href="../../assets/ico/favicon.ico"> -->
+
+    <title>ALSmith y Asociados</title>
+    <link href='http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,700|Alegreya+Sans:100,400,700,900,100italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+    <!-- Custom styles for this template -->
+        <link rel="stylesheet" href="_/css/main.css">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="bower_components/normalize-css/normalize.css">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css">
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <!-- HEADER -->
+    <?php include '_/php/header.php';?>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+
+
+<main> 
+    <?php include '_/php/jumbotron.php';?>
+ 
+
+      <!-- Example row of columns -->
+      <div class="row content">
+        <div class="nav nav-stacked affix-top col-xs-12 col-sm-3 col-sm-offset-0
+                                          col-md-2 col-md-offset-1" id="side-nav">
+          <div class="list-group   nav nav-stacked" >
+            <a href="#" class="list-group-item active">Busqueda</a>
+            <a href="#como" class="list-group-item">Alineacion</a>
+            <a href="#experiencia" class="list-group-item">Evaluacion</a>
+            <a href="#valores" class="list-group-item">Descripcion de puesto</a>
+            <a href="#" class="list-group-item ">Busqueda</a>
+            <a href="#como" class="list-group-item">Alineacion</a>
+            <a href="#experiencia" class="list-group-item">Evaluacion</a>
+            <a href="#valores" class="list-group-item">Descripcion de puesto</a>
+            <a href="#" class="list-group-item ">Busqueda</a>
+            <a href="#como" class="list-group-item">Alineacion</a>
+            <a href="#experiencia" class="list-group-item">Evaluacion</a>
+            <a href="#valores" class="list-group-item">Descripcion de puesto</a>            
+          </div> <!-- list group -->
+        </div>
+
+      <section class="descropcion-wrapper">
+        <div class="descripcion ">
+          
+            <div class="explicacion col-sm-7 col-sm-offset-3" id="que-hacemos">
+            <h2>Que hacemos?</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+           
+          </div> <!-- explicacion -->
+        </div><!-- descripcion -->
+        <div class="descripcion ">
+            <div class="explicacion col-sm-7 col-sm-offset-3" id="como">
+            <h2>Como lo hacemos?</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+           
+          </div> <!-- explicacion -->
+        </div><!-- descripcion -->
+        <div class="descripcion ">
+          
+            <div class="explicacion col-sm-7 col-sm-offset-3" id="experiencia">
+            <h2>Experiencia</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+           
+          </div> <!-- explicacion -->
+          </div><!-- descripcion -->
+        <div class="descripcion ">
+          
+            <div class="explicacion col-sm-7 col-sm-offset-3" id="valores">
+            <h2>Valores</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+           
+          </div> <!-- explicacion -->
+        </div><!-- descripcion -->
+      </section>
+
+      </div><!-- row content -->
+
+
+<!-- 
+      <footer class="navbar-bottom" style="position: fixed;">
+        <p>© ALSmith y Associados</p>
+      </footer> -->
+    </div> <!-- /container -->
+</main>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  
+
+</body></html>
