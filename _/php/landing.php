daf0100b919d8ca0e27be4a9c157b952447bbb5f
fdiazsmith <risd@Fernandos-MacBook-Pro.local>
2014-03-24 23:04:04 -0400

    landing page

---
+++ _/php/landing.php
@@ -0,0 +1,29 @@
+<div class="landing-wrapper">
+	<div class="background">
+		<div class="logo">
+<!-- 			<div class="al">AL</div>
+			<div class="smith">Smith</div>
+			<div class="associados">y Associados Consultores</div> -->
+			<img src="imgs/logo.jpg" alt="">
+		</div> <!-- logo -->
+		<div class="row">
+			<div class="content col-sm-4">
+				<h3 class="contacto-title"><span class="land-title">Telefonos</span></h3>
+				<div class="land-line"></div>
+				<p><strong>officina</strong> <a class="tel" href="tel:+52442281414">442.228.1414</a></p>
+				<p><strong>celular</strong> <a class="tel" href="tel:+5214422194682">442.219.4682</a></p>
+			</div>
+			<div class="content col-sm-4">
+				<h3 class="contacto-title"><span class="land-title">Email</span></h3>
+				<div class="land-line"></div>
+				<p><strong>Ana Luz</strong> <a href="mailto:alsmith@alsmith.com.mx" target="_top"> alsmith@alsmith.com.mx</a></p>
+				<p>	<strong>Curriculums</strong> <span>alsmith@alsmith.com.mx</span></p>
+			</div> <!-- dir -->
+			<div class="content col-sm-4">
+				<h3 class="contacto-title"><span class="land-title">Direccion</span></h3>
+				<div class="land-line"></div>
+				<p><span>officina</span> <span>Colinas del Bosque 2 Secc,<br>Corregidora, Qro</span></p>
+			</div> <!-- electronic -->
+		</div>
+	</div> <!-- background-->
+</div> <!-- landing-wrapper -->
