d6e0f1d0e69365823cc4748860b7172d97b6142c
fdiazsmith <risd@Fernandos-MacBook-Pro.local>
2014-05-29 09:14:06 -0400

    brings back form the world of lost php

--- _/js/main.js
+++ _/js/main.js
@@ -1,61 +1,139 @@
+
+/*
+*	V A R I A B L E S 
+*
+*/
 var $window = $(window)
  , winHeight;
 
 
-$(window).load(function () {
-	winHeight = $window.height();
-	$(".landing-wrapper").css("min-height" , winHeight+"px");
-	$(".landing-wrapper .background").animate({"opacity" : "1"}, "slow");
-	//for now when click anywhere in the page. it just fades out.
-	$(".landing-wrapper").on("click", function(){
-		$(".landing-wrapper").fadeOut();
-		$('main').css('display','block');
-	});
-	
-	$('.list-group').on('click', 'a', function(event){
-		scrollTo($(this));
-		// var sec = $(this).attr("href");
-		// var pos = $(this).data("pos");
-		// $('.active').removeClass("active");
-		// $(this).addClass("active");
-		// console.log(sec,pos);
-		// window.scrollTo(0,pos);
-		// event.preventDefault();
 
-	});
-	$('#nav-servicios').on('click', 'a', function(){
-			scrollTo($(this));
-	});
-	initThings();
-
-console.log("offset header");
-	$window.scroll(function(){
-		console.log($window.scrollTop());
-	});
-});
+/*
+*	F U N C T I O N S 
+*
+*/
 
 function initThings(){
+	//first load the components
+	loadComponents();
+	// adjustParagraphHeight();
+
+
 	if($('main').hasClass('hide-on-load')){
-	$('main').css("display", "none");		
-	}
+	// $('main').css("display", "none");
+	 }
 	$('#nav-servicios').height(winHeight);
 	$('.landing-wrapper').height(winHeight);
-	offsetHeaderHeight();
+
+
 }
+
 function scrollTo(where){
 	//fyi: 'where' needs to be a jquery object
 	event.preventDefault();
-	var scrollTarget = where.attr("href"),
-		yPosTarget = $('body').find(scrollTarget).position().top,
+	var yPosTarget = $('body').find(where.attr("href")).position().top,
 		headerHeight = $('header').height();
 
-console.log(yPosTarget);
+	console.log(yPosTarget);
 	var goToTarget = function(){
 		$('body,html').animate({scrollTop: yPosTarget},1000);
-		// -headerHeight.. sometime is necesary to use this. 
+		// - (substract) headerHeight.. sometime is necesary to use this. 
 	}();
 }
 
-function offsetHeaderHeight(){
-	$('main').css('margin-top', $('header').height()+'px' );
+//does not work it is being dealt 
+function adjustParagraphHeight(){
+	$('.descripcion').each(function(){
+		var thisH = $(this).contents();
+		for (var i = thisH.length - 1; i >= 0; i--) {
+			var h = thisH[i].height
+			,	totalH = totalH + h;
+		};
+		console.log(thisH, totalH);
+	});
+	console.log($('.descripcion').height());
 }
+
+//loads the components that will be reused through out into the site.
+// e.g. the header. 
+function loadComponents(){
+  $.ajax('_/components/header.html', {
+    success: function(response){
+      $("header").html(response);
+    },
+    error: function(request, errorType, errorMessage){
+    },
+
+    timeOut: 3000
+  });
+
+  //   $.ajax('_/components/landing.html', {
+  //   success: function(response){
+	 //    $(".landing").html(response);
+	 //    //once the response is in. then  format it.
+	 //  	$(".landing-wrapper").css("height" , winHeight+"px");
+	 //  	// console.log($(".landing-wrapper"));
+		// $(".landing-wrapper .background").animate({"opacity" : "1"}, "slow");
+  //   },
+  //   error: function(request, errorType, errorMessage){
+  //   },
+  //   timeOut: 3000
+  // });
+$.ajax('_/components/jumbotron.html', {
+    success: function(response){
+      $(".jumbotron-container").html(response);
+    },
+    error: function(request, errorType, errorMessage){
+    },
+    timeOut: 3000
+  });
+}
+
+
+/*
+*	H A N D L E R S
+*
+*/
+
+$(document).ready(function(){
+	winHeight = $window.height();
+	initThings();
+	console.log("this is before");
+
+
+	
+	//for now when click anywhere in the page. it just fades out.
+
+	$(".landing").on("click", function(){
+		console.log("landinf click");
+		$(".landing-wrapper").fadeOut();
+		$('main').css('display','block');
+	});
+
+	/////go to places.
+	$('.list-group').on('click', 'a', function(event){
+		scrollTo($(this));
+	});
+
+	$('#nav-servicios').on('click', 'a', function(){
+			scrollTo($(this));
+	});
+
+
+	console.log("offset header");
+
+	$window.scroll(function(){
+		console.log($window.scrollTop());
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
