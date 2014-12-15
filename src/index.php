4b35d24bd71fb18047e1b32df579ab02e7c5312c
Fernando Diaz Smith <fdiazsmith@gmail.com>
2014-12-14 20:54:45 -0500

    Blank Template

--- src/index.php
+++ src/index.php
@@ -1,122 +1,17 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!-- <link rel="shortcut icon" href="../../assets/ico/favicon.ico"> -->
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-    <title>ALSmith y Asociados</title>
-    <link href='http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,700|Alegreya+Sans:100,400,700,900,100italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
-    <!-- Custom styles for this template -->
-        <link rel="stylesheet" href="_/css/main.css">
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="bower_components/normalize-css/normalize.css">
-    <!-- <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css"> -->
-    <link rel="stylesheet" href="_/css/bootstrap-min.css">
-
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body style="overflow-x: hidden;" data-spy="scroll" data-target=".nav-stacked">
-    <?php include '_/php/landing.php';?>
-    <!-- HEADER -->
-    <?php include '_/php/header.php';?>
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-
-
-<main class="hide-on-load" style="overflow-x: hidden;" > 
-    <?php include '_/php/jumbotron.php';?>
- 
-
-      <!-- Example row of columns -->
-    <div class="row content" >
-        <div class="nav nav-stacked  col-xs-12 col-sm-3 col-sm-offset-0
-                                          col-md-2 col-md-offset-1 " id="side-nav"
-                                          data-spy="affix" data-offset-top="0">
-          <div class="list-group   nav nav-stacked" id="sideNav">
-            <a href="#que-hacemos" class="list-group-item">Que hacemos? </a>
-            <a href="#como" class="list-group-item" >Como lo hacemos?</a>
-            <a href="#experiencia" class="list-group-item">Experience</a>
-            <a href="#valores" class="list-group-item">Valores</a>
-          </div> <!-- list group -->
-        </div>
-
-      <section class="descropcion-wrapper" >
-        <div class="descripcion " id="que-hacemos">
-          <div class="explicacion col-sm-7 col-sm-offset-3" >
-            <h2>¿Que hacemos?</h2>
-            <article>
-              <p>Somos una empresa consultora en recursos humanos que lo asesora en la búsqueda del candidato ideal para su empresa.</p>
-              <p>En ALSmith y Asociados nos diferenciamos al ofrecer un servicio personalizado, directo, claro y adaptado a las necesidades específicas de su empresa.</p>
-              <p>Sabemos que nuestros clientes cuentan con todos los recursos y herramientas para definir qué soluciones funcionarían mejor para ellos y nosotros creemos que nuestro trabajo como asesores a sus necesidades de reclutamiento se basa en identificar con claridad estos conocimientos internos para ofrecer soluciones con una compatibilidad máxima y por tanto duradera. </p>
-              <p>ALSmith y Asociados le ayuda a crecer encontrando el talento adecuado que mejor se adaptará a la cultura de su organización.</p>
-            </article>
-          </div> <!-- explicacion -->
-        </div><!-- descripcion -->
-        <div class="descripcion " id="como">
-          <div class="explicacion col-sm-7 col-sm-offset-3">
-            <h2>¿Cómo lo hacemos?</h2>
-            <article>
-              <p>Garantizamos una relación de negocios confidencial tanto con los candidatos potenciales como con las empresas que nos contratan.</p>
-              <p>Mantenemos un trato personalizado y humano enmarcado en la sensibilidad humana, intuición y experiencia  de nuestros asociados y colaboradores.</p>
-              <p>Nos aseguramos que todas las búsquedas partan de investigación original. Entendiendo las realidades de cada empresa como únicas, reconocemos la importancia de una investigación moldeada a sus necesidades específicas. Evitamos buscar a candidatos en banco de datos.</p>
-            </article>
-          </div> <!-- explicacion -->
-        </div><!-- descripcion -->
-        <div class="descripcion " id="experiencia">
-          <div class="explicacion col-sm-7 col-sm-offset-3" >
-            <h2>Experiencia</h2>
-            <article>
-              <p>Con más de 15 años de experiencia en el campo y una metodología exhaustiva y personalizada tendrá la certeza de estar trabajando con un equipo de profesionales que encontrará el mejor talento para sus necesidades. </p>
-              <p>Los años de experiencia, calidad humana y transparencia de ALSmith y Asociados son su mejor solución. No ofrecemos candidatos perfectos pero sí candidatos que podrán crecer y hacer crecer su organización (compatibilidad máxima).</p>
-              <p>En ALSmith y Asociados es importante que las ofertas de trabajo signifiquen una verdadera oportunidad de desarrollo para el candidato y beneficio para la empresa. La experiencia nos ha demostrado que esto significa una larga y fructífera relación laboral entre cliente y ejecutivo contratado. </p>
-            </article>
-          </div> <!-- explicacion -->
-        </div><!-- descripcion -->
-        <div class="descripcion " id="valores">
-          <div class="explicacion col-sm-7 col-sm-offset-3" >
-            <h2>Valores</h2>
-            <article>
-              <p>El entendimiento por completo de lo que nuestros clientes requieren en el marco de sus estrategias y cultura organizacional. Cuidado del candidato para que ambos se beneficien en el largo plazo. </p>
-              <p>Análisis de todos los aspectos de un candidato (valores personales, competencias, creencias, actitudes, conductas y conocimientos), para una compatibilidad máxima.</p>
-              <p>Igualdad de oportunidades para candidatos que cumplan con las competencias del puesto sin hacer ningún tipo de discriminación por factores que no son inherentes a la descripción del puesto de nuestros clientes. </p>
-              <p>Honestidad absoluta con nuestros clientes y candidatos en cuanto a avances, candidatos, situación del mercado, dificultades y/o cualquier información que pudiera ayudarles a tomar decisiones.</p>
-            </article>
-          </div> <!-- explicacion -->
-        </div><!-- descripcion -->
-      </section>
-
-    </div><!-- row content -->
-
-
-<!-- 
-      <footer class="navbar-bottom" style="position: fixed;">
-        <p>© ALSmith y Associados</p>
-      </footer> -->
-    </div> <!-- /container -->
-</main>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="_/js/main.js"></script>
-  
-
-</body></html>
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
