2db825a27538e3713a39bc92d1babe99e765d661
Fernando Diaz Smith <fdiazsmith@gmail.com>
2014-12-16 09:49:50 -0500

    functions pull in other functions

---
+++ src/wp-content/themes/Alsmith/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+if( !defined( 'ABSPATH' ) ) exit; // exit if accessed directly
+
+/**
+ *
+ * Alsmith.com.mx THEME FUNCTIONS
+ *
+ * Don't edit this file directly!
+ * Instead, edit each included file as needed.
+ *
+ */
+
+$template_directory = get_template_directory();
+
+require( $template_directory . '/includes/basic-setup.php' );
+
+// require( $template_directory . '/includes/custom-page.php' );
+
+?>
