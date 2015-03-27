f2d4cf8e6ed4534bfb0c469321d19693802cf677
Fernando Diaz Smith <fdiazsmith@gmail.com>
2015-03-26 22:11:46 -0400

    Change the default side bar to contact info

--- src/wp-content/themes/Alsmith/sidebar.php
+++ src/wp-content/themes/Alsmith/sidebar.php
@@ -3,36 +3,26 @@
     <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('Sidebar Widgets')) : else : ?>
     
         <!-- All this stuff in here only shows up if you DON'T have any widgets active in this zone -->
-
-    	<?php get_search_form(); ?>
-    
-    	<?php wp_list_pages('title_li=<h2>Pages</h2>' ); ?>
-    
-    	<h2>Archives</h2>
-    	<ul>
-    		<?php wp_get_archives('type=monthly'); ?>
-    	</ul>
-        
-        <h2>Categories</h2>
-        <ul>
-    	   <?php wp_list_categories('show_count=1&title_li='); ?>
-        </ul>
-        
-    	<?php wp_list_bookmarks(); ?>
-    
-    	<h2>Meta</h2>
-    	<ul>
-    		<?php wp_register(); ?>
-    		<li><?php wp_loginout(); ?></li>
-    		<li><a href="http://wordpress.org/" title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress</a></li>
-    		<?php wp_meta(); ?>
-    	</ul>
-    	
-    	<h2>Subscribe</h2>
-    	<ul>
-    		<li><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a></li>
-    		<li><a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a></li>
-    	</ul>
+        <?php
+        $slug_to_get = 'contacto';
+        $args=array(
+        'name' => $slug_to_get,
+        'post_type' => 'page',
+        'post_status' => 'publish',
+        'showposts' => 1,
+        'caller_get_posts'=> 1
+        );
+        $my_posts = get_posts($args);
+        if( $my_posts ) {
+        echo 'ID on the first post found '.$my_posts[0]->ID;
+        }
+        ?>
+            <div> <?php  the_field("email",$my_posts[0]->ID); ?>       </div>
+            <div> <?php  the_field("email_cv", $my_posts[0]->ID); ?>    </div>
+            <div> <?php  the_field("tel", $my_posts[0]->ID); ?>          </div>
+            <div> <?php  the_field("cel", $my_posts[0]->ID); ?>              </div>
+            <div> <?php  the_field("text_cv", $my_posts[0]->ID); ?>         </div>
+            <div> <?php  the_field("extra_info", $my_posts[0]->ID);  ?>  </div>
 	
 	<?php endif; ?>
 
