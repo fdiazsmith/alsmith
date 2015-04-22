9ec7b5453daf76583d282fae79b4063fc3056c15
Fernando Diaz Smith <fdiazsmith@gmail.com>
2015-04-21 21:19:33 -0700

    inquiry into sidebar

--- src/wp-content/themes/Alsmith/sidebar.php
+++ src/wp-content/themes/Alsmith/sidebar.php
@@ -4,18 +4,15 @@
     
         <!-- All this stuff in here only shows up if you DON'T have any widgets active in this zone -->
         <?php
-        $slug_to_get = 'contacto';
-        $args=array(
-        'name' => $slug_to_get,
-        'post_type' => 'page',
-        'post_status' => 'publish',
-        'showposts' => 1,
-        'caller_get_posts'=> 1
-        );
-        $my_posts = get_posts($args);
-        if( $my_posts ) {
-        echo 'ID on the first post found '.$my_posts[0]->ID;
-        }
+            $slug_to_get = 'contacto';
+            $args=array(
+            'name' => $slug_to_get,
+            'post_type' => 'page',
+            'post_status' => 'publish',
+            'showposts' => 1,
+            'caller_get_posts'=> 1
+            );
+            $my_posts = get_posts($args);
         ?>
             <div> <?php  the_field("email",$my_posts[0]->ID); ?>       </div>
             <div> <?php  the_field("email_cv", $my_posts[0]->ID); ?>    </div>
